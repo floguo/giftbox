0880cf24ffc3ee6a4843e9e748893c99bbc8314e
eugenechantk <me@eugenechantk.me>
2024-12-23 23:19:06 +0800

    fix build

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,8 +6,7 @@ import Share from "./share-menu";
 import { Button } from "./ui/button";
 
 export const Header = () => {
-  const { isEditable, removeIsLetterShowed, setIsLetterShowed } =
-    useAppContext();
+  const { isEditable, removeIsLetterShowed } = useAppContext();
   const router = useRouter();
   return (
     <div className="w-full p-4 flex justify-between items-center absolute top-0 left-0 z-30">
